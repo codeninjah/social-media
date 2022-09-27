b95ae34e2fd8c7b4fe6404671e6120b817142ef9
codeninjah <codeninjahblog@gmail.com>
2022-09-27 10:36:04 +0200

    Work in progress

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
+app.use(express.json())
+
+
+app.use("/api/user", routes.user);
+app.use("/api/tasks", routes.message);
 
 app.listen(8080, function() {
     console.log("Server is running...")
